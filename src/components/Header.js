ae0c4b4696e2e191264aec09f1df9c6f7c83ca65
SlaviaF <slaviafurtado@gmail.com>
2022-02-23 14:50:36 +0100

    added burger logo title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,14 @@ import "./Header.css"
 import { Link } from "react-router-dom";
 import bobBanner from "../images/bob-banner.png";
 import bobLogo from "../images/bobs-burgers-logo.png";
+import burgerTitle from "../images/burger-title.png";
 const Header = () => {
   return (
     <>
       <div className="top-nav">
         <div>
           <img src={bobLogo} alt="Bob's burger logo" />
+          <img  className="burgerTitle"src={burgerTitle} alt="Bob's burger logo title" />
         </div>
         <ul className="nav-ul">
           <li>
