11b742ff035c0ea10ea69c50adcf6ab00d1716cb
SlaviaF <slaviafurtado@gmail.com>
2022-02-23 14:49:28 +0100

    deleted the commented out code

--- src/components/FetchEpisodes.js
+++ src/components/FetchEpisodes.js
@@ -1,64 +1,20 @@
 //import React,{useState, useEffect} from 'react'
-import DisplayEpisodes from './DisplayEpisodes';
-import useFetch from '../useFetch';
-
-
-/*
-const FetchEpisodes = () => {
-  
-  const bobsApi = "https://bobsburgers-api.herokuapp.com/episodes"  
-  const [error, setError] = useState(null);
-  const [data, setData] = useState([]);
-  const [isloading, setIsLoading] = useState(false);
-
-
-  const fetchMealWithId = async () => {
-    try {
-      setIsLoading(true);
-      setError(null);
-      const response = await fetch(bobsApi);
-      if (!response.ok) {
-        const message = `An error has occured : ${response.statusText}`;
-        throw Error(message);
-      } else {
-        const apiData = await response.json();
-        setData(apiData);
-        setIsLoading(false);
-      }
-    } 
-    catch (error) {
-      setIsLoading(false);
-      setError(error.message);
-    }
-  };
-  useEffect(() => {
-    fetchMealWithId();
-  }, []);
-
-    data && console.log(data)
-  return (
-    <div>
-        {isloading && <h1>Loading...</h1>}
-        <div className='container'>
-        <DisplayEpisodes episodes = {data}/>
-        </div>
-    </div>
-  )
-}*/
-
+import DisplayEpisodes from "./DisplayEpisodes";
+import useFetch from "../useFetch";
 
 const FetchEpisodes = () => {
-  const { data, isLoading, error } = useFetch(`https://bobsburgers-api.herokuapp.com/episodes`);
+  const { data, isLoading, error } = useFetch(
+    `https://bobsburgers-api.herokuapp.com/episodes`
+  );
   return (
     <div>
-    {isLoading && <h1 className="loader">Loading...</h1>}
-    {error && <h3> Error: Something went wrong</h3>}
-    <div className='container'>
-        <DisplayEpisodes episodes = {data}/>
+      {isLoading && <h1 className="loader">Loading...</h1>}
+      {error && <h3> Error: Something went wrong</h3>}
+      <div className="container">
+        <DisplayEpisodes episodes={data} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FetchEpisodes
-
+export default FetchEpisodes;
